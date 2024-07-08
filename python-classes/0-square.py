bce94f173d1fab1624fb4d82d564b5f4e14a9e8d
Cynthia2315 <c.keza@alustudent.com>
2024-07-08 03:26:39 -0700

    classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This code contains blank class
+"""
+
+
+class Square():
+    """
+    CLASS IS A SQUARE CLASS AND IS EMPTY
+    """
---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+DOCUMENTATION FOR MODULES IN HERE
+"""
+
+
+class Square:
+    """
+    CLASS CREATES A PRIVATE SIZE INSTANCE VARIABLE AND ASSIGNS IT
+    """
+    def __init__(self, size):
+        self._Square__size = size
